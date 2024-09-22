03dff56e24a15a82a900fb427453b83ff7142df0
JDPayne11 <paynejohn888@gmail.com>
2024-09-22 00:18:53 -0400

    Removed venv

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
+import pandas as pd
 
-url = f'https://eodhd.com/api/exchange-symbol-list/{EXCHANGE_CODE}?api_token=66ef5afcc7de75.85426574&fmt=json'
-data = requests.get(url).json()
-
-print(data)
+url = f'https://eodhd.com/api/exchange-symbol-list/US?api_token=66ef5afcc7de75.85426574&fmt=json'
+r = requests.get(url).json()
+data = r.text
+df = pd.read_json(data)
+df
